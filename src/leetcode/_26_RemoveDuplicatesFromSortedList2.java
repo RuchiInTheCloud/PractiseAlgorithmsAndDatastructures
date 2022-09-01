7deb51ec371476bbd405b3742121dd5117738643
D064288 <ruchi.dhiman@sap.com>
2022-09-01 19:47:24 +0200

    Add file

---
+++ src/leetcode/_26_RemoveDuplicatesFromSortedList2.java
@@ -0,0 +1,74 @@
+package leetcode;
+
+/*
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+Example 1:
+Input: head = [1,2,3,3,4,4,5]
+Output: [1,2,5]
+
+Example 2:
+Input: head = [1,1,1,2,3]
+Output: [2,3]
+
+Constraints:
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+ */
+public class _26_RemoveDuplicatesFromSortedList2 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            while (true) {
+                boolean hasDuplicate = false;
+                while (head != null && head.next != null && head.val == head.next.val) {
+                    head = head.next;
+                    hasDuplicate = true;
+                }
+
+                if (hasDuplicate) {
+                    head = head.next;
+                }
+
+                if (head != null && head.next != null && head.val != head.next.val) {
+                    break;
+                } else if (head == null || head.next == null) {
+                    break;
+                }
+            }
+
+            if (head != null) {
+                ListNode prev = head;
+                ListNode current = head.next;
+                while (current != null) {
+                    boolean hasDuplicate = false;
+                    while (current != null && current.next != null && current.val == current.next.val) {
+                        current = current.next;
+                        hasDuplicate = true;
+                    }
+
+                    if (hasDuplicate) {
+                        current = current.next;
+                        prev.next = current;
+                        continue;
+                    }
+
+                    prev.next = current;
+                    prev = current;
+                    current = current.next;
+                }
+            }
+            return head;
+        }
+    }
+}
