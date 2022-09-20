d448852207a14f1f5e583c516920122a91ad65b0
D064288 <ruchi.dhiman@sap.com>
2022-09-20 17:36:13 +0200

    Add file

--- src/leetcode/_66_SurroundedRegions.java
+++ src/leetcode/_66_SurroundedRegions.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
 Given an m x n matrix board containing 'X' and 'O', capture all regions that are 4-directionally surrounded by 'X'.
 
@@ -25,7 +28,56 @@ n == board[i].length
 board[i][j] is 'X' or 'O'.
  */
 public class _66_SurroundedRegions {
-    public void solve(char[][] board) {
+    class Solution {
+        class Coordinate {
+            int x;
+            int y;
 
+            Coordinate(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        public void solve(char[][] board) {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (i == 0 || j == 0 || i == board.length - 1 || j == board[0].length - 1) {
+                        if (board[i][j] == 'O') {
+                            bfs(board, i, j);
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (board[i][j] == 'O') {
+                        board[i][j] = 'X';
+                    } else if (board[i][j] == 'E') {
+                        board[i][j] = 'O';
+                    }
+                }
+            }
+        }
+
+        private void bfs(char[][] board, int i, int j) {
+            int[][] delta = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+            Queue<Coordinate> queue = new LinkedList<>();
+            addNeighbour(queue, i, j, board);
+
+            while (!queue.isEmpty()) {
+                Coordinate coordinate = queue.remove();
+                for (int k = 0; k < delta.length; k++) {
+                    addNeighbour(queue, coordinate.x + delta[k][0], coordinate.y + delta[k][1], board);
+                }
+            }
+        }
+
+        private void addNeighbour(Queue<Coordinate> queue, int x, int y, char[][] board) {
+            if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && board[x][y] == 'O') {
+                queue.add(new Coordinate(x, y));
+                board[x][y] = 'E';
+            }
+        }
     }
 }
--- src/leetcode/_68_Subsets.java
+++ src/leetcode/_68_Subsets.java
@@ -1,4 +1,47 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Example 2:
+Input: nums = [0]
+Output: [[],[0]]
+
+Constraints:
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+ */
 public class _68_Subsets {
+    class Solution {
+        public List<List<Integer>> subsets(int[] nums) {
+            int size = 1 << nums.length;
+            List<List<Integer>> subsets = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = createSubset(nums, i);
+                subsets.add(subset);
+            }
+            return subsets;
+        }
+
+        private List<Integer> createSubset(int[] nums, int num) {
+            int index = 0;
+            List<Integer> subset = new LinkedList<>();
+            for (int i = num; i != 0; i = (i >> 1)) {
+                if ((i & 1) != 0) {
+                    subset.add(nums[index]);
+                }
+                index++;
+            }
+            return subset;
+        }
+    }
 }
