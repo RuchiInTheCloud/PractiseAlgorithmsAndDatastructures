cd6152e1c4896860ce64fe74702e18e76a047063
D064288 <ruchi.dhiman@sap.com>
2022-10-16 23:51:05 +0200

    Add file

--- src/leetcode/_21_MultiplyStrings.java
+++ src/leetcode/_21_MultiplyStrings.java
@@ -25,89 +25,48 @@ Both num1 and num2 do not contain any leading zero, except the number 0 itself.
 public class _21_MultiplyStrings {
     class Solution {
         public String multiply(String num1, String num2) {
-            List<String> subProductList = new LinkedList<>();
-
-            for (int i = num2.length() - 1; i >= 0; i--) {
-                char num2Char = num2.charAt(i);
-                int num2Digit = convertCharToDigit(num2Char);
-                if (num2Digit == -1) {
-                    return null;
-                }
-
-                StringBuffer productSb = new StringBuffer();
-                int carry = 0;
-
-                int digitIndex = num1.length() - 1;
-                while (digitIndex >= 0) {
-                    char num1Char = num1.charAt(digitIndex);
-                    int num1Digit = convertCharToDigit(num1Char);
-                    if (num1Digit == -1) {
-                        return null;
-                    }
-                    int product = num1Digit * num2Digit;
-                    int digit = (product + carry) % 10;
-                    productSb.append(digit);
-                    carry = (product + carry) / 10;
-                    digitIndex -= 1;
-                }
-                if (carry > 0) {
-                    productSb.append(carry);
-                }
-
-                int[] zeros = new int[num2.length() - 1 - i];
-                productSb.reverse().append(Arrays.toString(zeros).replace(",", "").replace("[", "").replace("]", "")
-                        .replace(" ", "").trim());
-
-                subProductList.add(productSb.toString());
+            if (num1.equals("0") || num2.equals("0")) {
+                return String.valueOf(0);
             }
 
-            String sum = subProductList.get(0);
-            for (int i = 1; i < subProductList.size(); i++) {
-                sum = add(sum, subProductList.get(i));
+            int len1 = num1.length() - 1;
+            int len2 = num2.length() - 1;
+            int i1, i2;
+            i1 = len1;
+            i2 = len2;
+
+            int a;
+            int b;
+
+            int[] num = new int[i1 + i2 + 2];
+            Arrays.fill(num, 0);
+            while (i1 >= 0) {
+                a = i1 >= 0 ? num1.charAt(i1) - '0' : 0;
+                b = i2 >= 0 ? num2.charAt(i2) - '0': 0;
+                num[len1 + len2 - i1 - i2] += (a * b) % 10;
+                num[len1 + len2 - i1 - i2 + 1] += (a * b) / 10;
+
+                i2 -= 1;
+                if (i2 == -1) {
+                    i1 -= 1;
+                    i2 = num2.length() - 1;
+                }
             }
 
-            int[] zeros = new int[sum.length()];
-            String zero = Arrays.toString(zeros).replace(",", "").replace("[", "").replace("]", "").replace(" ", "")
-                    .trim();
-            return sum.equals(zero) ? "0" : sum;
-        }
-
-        public int convertCharToDigit(char c) {
-            if (c < '0' || c > '9') {
-                return -1;
-            }
-            return c - '0';
-        }
-
-        public String add(String num1, String num2) {
-            int num1Index, num2Index;
-            num1Index = num1.length() - 1;
-            num2Index = num2.length() - 1;
-
-            int digitA, digitB;
+            StringBuffer numString = new StringBuffer();
+            int sum = 0;
             int carry = 0;
-
-            StringBuilder result = new StringBuilder();
-
-            while (num1Index >= 0 || num2Index >= 0) {
-                digitA = num1Index >= 0 ? convertCharToDigit(num1.charAt(num1Index)) : 0;
-                digitB = num2Index >= 0 ? convertCharToDigit(num2.charAt(num2Index)) : 0;
-
-                if (digitA == -1 || digitB == -1) {
-                    return null;
+            for (int i = 0; i < num.length; i++) {
+                if (i == num.length - 1 && num[i] == 0) continue;
+                if (num[i] > 9) {
+                    sum = num[i] % 10;
+                    carry = num[i] / 10;
+                    num[i] = sum;
+                    num[i + 1] += carry;
                 }
-
-                result.append(((digitA + digitB + carry) % 10));
-                carry = (digitA + digitB + carry) / 10;
-                num1Index--;
-                num2Index--;
+                numString.append(String.valueOf(num[i]));
             }
-
-            if (carry > 0) {
-                result.append(carry);
-            }
-
-            return result.reverse().toString();
+            return numString.reverse().toString();
         }
     }
 }
