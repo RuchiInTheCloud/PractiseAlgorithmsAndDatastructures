544ef393c7c442fd258f89a85a810b52475aa85a
D064288 <ruchi.dhiman@sap.com>
2022-10-20 17:40:36 +0200

    Add file

--- src/leetcode/_66_SurroundedRegions.java
+++ src/leetcode/_66_SurroundedRegions.java
@@ -29,55 +29,34 @@ board[i][j] is 'X' or 'O'.
  */
 public class _66_SurroundedRegions {
     class Solution {
-        class Coordinate {
-            int x;
-            int y;
-
-            Coordinate(int x, int y) {
-                this.x = x;
-                this.y = y;
-            }
-        }
-
         public void solve(char[][] board) {
             for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[0].length; j++) {
-                    if (i == 0 || j == 0 || i == board.length - 1 || j == board[0].length - 1) {
-                        if (board[i][j] == 'O') {
-                            bfs(board, i, j);
-                        }
+                for (int j = 0; j < board[i].length; j++) {
+                    if ((i == 0 || j == 0 || i == board.length - 1 || j == board[i].length - 1) && board[i][j] == 'O') {
+                        findConnectedRegion(board, i, j);
                     }
                 }
             }
             for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[0].length; j++) {
+                for (int j = 0; j < board[i].length; j++) {
                     if (board[i][j] == 'O') {
                         board[i][j] = 'X';
-                    } else if (board[i][j] == 'E') {
+                    } else if (board[i][j] == 'U') {
                         board[i][j] = 'O';
                     }
                 }
             }
         }
 
-        private void bfs(char[][] board, int i, int j) {
-            int[][] delta = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-            Queue<Coordinate> queue = new LinkedList<>();
-            addNeighbour(queue, i, j, board);
-
-            while (!queue.isEmpty()) {
-                Coordinate coordinate = queue.remove();
-                for (int k = 0; k < delta.length; k++) {
-                    addNeighbour(queue, coordinate.x + delta[k][0], coordinate.y + delta[k][1], board);
-                }
+        private void findConnectedRegion(char[][] board, int i, int j) {
+            if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] == 'X' || board[i][j] == 'U') {
+                return;
             }
-        }
-
-        private void addNeighbour(Queue<Coordinate> queue, int x, int y, char[][] board) {
-            if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && board[x][y] == 'O') {
-                queue.add(new Coordinate(x, y));
-                board[x][y] = 'E';
-            }
+            board[i][j] = 'U';
+            findConnectedRegion(board, i - 1, j);
+            findConnectedRegion(board, i, j - 1);
+            findConnectedRegion(board, i, j + 1);
+            findConnectedRegion(board, i + 1, j);
         }
     }
 }
