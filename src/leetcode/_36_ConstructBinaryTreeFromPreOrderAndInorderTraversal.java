f6ddc5f0a09a3dcfe131bdd0f8a97e77d04684fa
D064288 <ruchi.dhiman@sap.com>
2022-10-30 18:13:25 +0100

    Add file

--- src/leetcode/_36_ConstructBinaryTreeFromPreOrderAndInorderTraversal.java
+++ src/leetcode/_36_ConstructBinaryTreeFromPreOrderAndInorderTraversal.java
@@ -21,4 +21,49 @@ preorder is guaranteed to be the preorder traversal of the tree.
 inorder is guaranteed to be the inorder traversal of the tree.
 */
 public class _36_ConstructBinaryTreeFromPreOrderAndInorderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public TreeNode buildTree(int[] preorder, int[] inorder) {
+            return buildTree(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+        }
+
+        private TreeNode buildTree(int[] preorder, int[] inorder, int pL, int pR, int iL, int iR) {
+            if (pL > pR || iL > iR) {
+                return null;
+            }
+
+            TreeNode root = new TreeNode(preorder[pL]);
+            int iM = searchIndex(inorder, preorder[pL]);
+            root.left = buildTree(preorder, inorder, pL + 1, pL + (iM - iL), iL, iM - 1);
+            root.right = buildTree(preorder, inorder, pL + (iM - iL) + 1, pR, iM + 1, iR);
+            return root;
+        }
+
+        private int searchIndex(int[] inorder, int val) {
+            for (int i = 0; i < inorder.length; i++) {
+                if (inorder[i] == val) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
 }
