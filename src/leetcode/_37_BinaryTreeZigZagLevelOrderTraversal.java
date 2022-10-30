ea9d7e53bb5d174306ace0096eb880d4ddf46bb6
D064288 <ruchi.dhiman@sap.com>
2022-10-30 21:35:38 +0100

    Add file

--- src/leetcode/_37_BinaryTreeZigZagLevelOrderTraversal.java
+++ src/leetcode/_37_BinaryTreeZigZagLevelOrderTraversal.java
@@ -1,7 +1,11 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
-Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to
+right, then right to left for the next level and alternate between).
 
 Example 1:
 Input: root = [3,9,20,null,null,15,7]
@@ -20,4 +24,62 @@ The number of nodes in the tree is in the range [0, 2000].
 -100 <= Node.val <= 100
  */
 public class _37_BinaryTreeZigZagLevelOrderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            List<List<Integer>> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+            List<Integer> resultLevel;
+            List<TreeNode> level = List.of(root);
+            List<TreeNode> newLevel;
+
+            boolean leftToRight = true;
+            int index, start, end, increment;
+
+            while (!level.isEmpty()) {
+                newLevel = new ArrayList<>();
+                resultLevel = new ArrayList<>();
+
+                start = leftToRight ? 0 : level.size() - 1;
+                end = leftToRight ? level.size() : -1;
+                increment = leftToRight ? 1 : -1;
+                for (index = start; index != end; index += increment) {
+                    resultLevel.add(level.get(index).val);
+                }
+                result.add(resultLevel);
+
+                for (TreeNode node : level) {
+                    if (node.left != null) {
+                        newLevel.add(node.left);
+                    }
+                    if (node.right != null) {
+                        newLevel.add(node.right);
+                    }
+                }
+                level = newLevel;
+                leftToRight = !leftToRight;
+            }
+            return result;
+        }
+    }
 }
