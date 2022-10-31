cb2c01d11ea36ac63e557dcd415504f4fc97f949
D064288 <ruchi.dhiman@sap.com>
2022-10-31 16:58:17 +0100

    Add file

--- src/leetcode/_44_BinarySearchTreeIterator.java
+++ src/leetcode/_44_BinarySearchTreeIterator.java
@@ -1,4 +1,57 @@
 package leetcode;
 
+import java.util.Stack;
+
 public class _44_BinarySearchTreeIterator {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class BSTIterator {
+        Stack<TreeNode> stack = new Stack<>();
+
+        public BSTIterator(TreeNode root) {
+            TreeNode curr = root;
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+        }
+
+        public int next() {
+            TreeNode res = stack.pop();
+            TreeNode curr = res.right;
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            return res.val;
+        }
+
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+    }
+
+    /**
+     * Your BSTIterator object will be instantiated and called as such:
+     * BSTIterator obj = new BSTIterator(root);
+     * int param_1 = obj.next();
+     * boolean param_2 = obj.hasNext();
+     */
 }
